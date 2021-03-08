5117134559ae256865812bbf0d959b0f1d80a6c8
lokeni21 <79953707+lokeni21@users.noreply.github.com>
2021-03-09 12:26:44 +1300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public bool buttonwasclicked = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void reset()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            dateTimePicker1.Value = DateTime.Now;
+            numericUpDown1.Value = 1;
+            numericUpDown2.Value = 1;
+            comboBox1.Text = "";
+            button1.Text = "Select";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            buttonwasclicked = true;
+            {
+                this.Hide();
+                var newform = new Form2();
+                if (newform.ShowDialog() == DialogResult.OK)
+
+                    if (newform.radioButton1.Checked == true)
+                        button1.Text = newform.radioButton1.Text;
+                    else if (newform.radioButton2.Checked == true)
+                        button1.Text = newform.radioButton2.Text;
+                    else if (newform.radioButton3.Checked == true)
+                        button1.Text = newform.radioButton3.Text;
+                    else if (newform.radioButton4.Checked == true)
+                        button1.Text = newform.radioButton4.Text;
+
+               
+            }
+
+               int num = 0;
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Name is required");
+                textBox1.BackColor = Color.Red;
+                textBox1.Focus();
+            }
+            else if (textBox2.Text == "")
+            {
+                textBox1.BackColor = Color.White;
+                MessageBox.Show("Surname must be filled");
+                textBox2.BackColor = Color.Red;
+                textBox2.Focus();
+            }
+            else if (textBox3.Text == "")
+            {
+                textBox4.BackColor = Color.White;
+                MessageBox.Show("Mobile number is required");
+                textBox3.BackColor = Color.Red;
+                textBox3.Focus();
+            }
+            else if (int.TryParse(textBox3.Text, out num) == false)
+            {
+                textBox4.BackColor = Color.White;
+                MessageBox.Show("Mobile can't contain any letters/symbols");
+                textBox3.BackColor = Color.Red;
+                textBox3.Focus();
+            }
+            else if (textBox4.Text == "")
+            {
+                textBox3.BackColor = Color.White;
+                MessageBox.Show("Email Address is required");
+                textBox4.BackColor = Color.Red;
+                textBox4.Focus();
+            }
+            else if (numericUpDown1.Value == 0)
+            {
+                textBox4.BackColor = Color.White;
+                MessageBox.Show("Please enter the hour time");
+                numericUpDown1.BackColor = Color.Red;
+                numericUpDown2.BackColor = Color.Red;
+            }
+            else if (comboBox1.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select a person");
+            }
+            else if (buttonwasclicked == false)
+            {
+                MessageBox.Show("Please select a meeting aim");
+            }
+            else
+            {
+                this.Hide();
+                var newform = new Form2();
+                if (newform.ShowDialog() == DialogResult.OK)
+
+                    if (newform.radioButton1.Checked == true)
+                        button1.Text = newform.radioButton1.Text;
+                    else if (newform.radioButton2.Checked == true)
+                        button1.Text = newform.radioButton2.Text;
+                    else if (newform.radioButton3.Checked == true)
+                        button1.Text = newform.radioButton3.Text;
+                    else if (newform.radioButton4.Checked == true)
+                        button1.Text = newform.radioButton4.Text;
+
+                this.Show();
+            }
+
+        }
+        private string buttonValue;
+        private string hourValue;
+        private string minuteValue;
+        private string theDate;
+        private int num_x;
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            theDate = dateTimePicker1.Value.ToShortDateString();
+            hourValue = Convert.ToString(numericUpDown1.Value);
+
+
+            if (button1.Text == "Select")
+                buttonValue = "";
+
+
+            else
+                buttonValue = button1.Text;
+
+
+
+            if (numericUpDown2.Value <= 9)
+                minuteValue = 0 + Convert.ToString(numericUpDown2.Value);
+
+
+            else
+                minuteValue = Convert.ToString(numericUpDown2.Value);
+
+            if (textBox1.Text == "")
+                listBox1.Items.Add("You need to enter a first name");
+
+
+            else if (textBox2.Text == "")
+                listBox1.Items.Add("You need to enter a surname");
+
+
+            else if (textBox3.Text == "")
+                listBox1.Items.Add("You need to enter a phone number");
+
+
+            else if (int.TryParse(textBox3.Text, out num_x) == false)
+            {
+                listBox1.Items.Add("You need to enter a valid phone number");
+                listBox1.Items.Add("(one with all digits)");
+            }
+
+
+            else if (textBox4.Text == "")
+                listBox1.Items.Add("You need to enter an email");
+            
+           
+
+            else if (!textBox4.Text.Contains("@"))
+            {
+                listBox1.Items.Add("You need to enter a valid email");
+                listBox1.Items.Add("(@ symbol required)");
+            }
+
+
+            else if (comboBox1.Text == "")
+                listBox1.Items.Add("You need to select a person that you want to have a meeting with");
+
+
+            else if (button1.Text == "Select")
+                listBox1.Items.Add("You need to select a meeting aim");
+
+
+            else
+                listBox1.Items.Add("Meeting For : " + textBox1.Text + "" + textBox2.Text + "" + textBox3.Text + "" + textBox4.Text);
+            listBox1.Items.Add("Meeting At : " + numericUpDown1.Value + ":" + numericUpDown2.Value + " " + comboBox1.SelectedItem);
+            listBox1.Items.Add("Meeting with : " + comboBox1.SelectedItem);
+            listBox1.Items.Add("Meeting Aim : " + button1.Text);
+            listBox1.Items.Add("---------------------------------------");
+            textBox1.Text = ("");
+            textBox2.Text = ("");
+            textBox3.Text = ("");
+            textBox4.Text = ("");
+            dateTimePicker1.ResetText();
+            numericUpDown1.ResetText();
+            numericUpDown2.ResetText();
+            comboBox1.ResetText();
+            button1.Text = ("Meeting Aim");
+
+
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+
+            
+        }
+    }
+}
+
+
+
---
+++ Form2.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(56, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(291, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Please specify the nature of the meeting";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(46, 69);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(63, 17);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Meeting";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(46, 163);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(65, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Site Visit";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(242, 69);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(113, 17);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Sales Appointment";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(242, 163);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(105, 17);
+            this.radioButton4.TabIndex = 4;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "student interview";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button1.Location = new System.Drawing.Point(46, 236);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Cancel";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(242, 236);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "OK";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form2
+            // 
+            this.AcceptButton = this.button2;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.button1;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.RadioButton radioButton1;
+        public System.Windows.Forms.RadioButton radioButton2;
+        public System.Windows.Forms.RadioButton radioButton3;
+        public System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
